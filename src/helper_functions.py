95d0525e311ae214406b72f1387d1547cf798bfa
aadideshpande <aadi.deshpande1@gmail.com>
2025-03-16 13:24:26 -0500

    add jupyter notebook for table 1 (mtm losses)

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -23,6 +23,18 @@ def GSIB_bank_id():
       451965] #wells fargo
     return GSIB
 
+def large_ex_GSIB_bank_id(large):
+    bank_id_large_ex_GSIB = []
+    for bank_id in large['rssd9001']:
+       bank_id_large_ex_GSIB.append(bank_id)
+    return bank_id_large_ex_GSIB
+
+def small_bank_id(small):
+    bank_id_small = []
+    for bank_id in small['rssd9001']:
+       bank_id_small.append(bank_id)
+    return bank_id_small
+
 def calculate_losses(treasury_prices, df_rmbs, df_loans, df_treasuries, df_other_loans, df_asset, rmbs_multiplier):
     start_date = "2022-03-31"
     end_date ="2023-03-31"
@@ -129,3 +141,77 @@ def calculate_losses(treasury_prices, df_rmbs, df_loans, df_treasuries, df_other
             'loss/gross_asset': -(total_loss / bank_total_asset) if bank_total_asset else 0,
         }
     return bank_losses_assets
+
+
+def calculate_uninsured_deposit_mm_asset(uninsured_deposit, bank_losses):
+    
+    # Initialize an empty list to store the results
+    results = []
+    
+    # Adjust the uninsured_deposit DataFrame to use both 'bank_name' and 'Bank_ID' as a multi-index for quick lookup
+    uninsured_lookup = uninsured_deposit.set_index(['rssd9001'])['uninsured_deposits'].to_dict()
+    
+    # Iterate over each row in bank_losses DataFrame
+    for index, bank_loss_row in bank_losses.iterrows():
+        bank_id = bank_loss_row['bank_ID']
+        
+        # Adjust the lookup to include 'Bank_ID'
+        uninsured_deposit_value = uninsured_lookup.get((bank_id), 0)
+        
+        # Calculate 'MM Asset' as the sum of 'total_loss' and 'gross_asset' (as defined in the paper)
+        mm_asset = bank_loss_row['total_loss'] + bank_loss_row['gross_asset']
+        
+        # Calculate Uninsured Deposit/MM Asset ratio 
+        if mm_asset > 0:
+            uninsured_deposit_mm_asset_ratio = uninsured_deposit_value / mm_asset
+        
+        # Append to final dataframe
+        results.append({
+            'bank_ID': bank_id, 
+            'total_loss': bank_loss_row['total_loss'], 
+            'total_asset': bank_loss_row['gross_asset'],
+            'mm_asset': mm_asset,
+            'uninsured_deposit': uninsured_deposit_value, 
+            'Uninsured_Deposit_MM_Asset': uninsured_deposit_mm_asset_ratio
+        })
+    
+    # Convert results list to DataFrame and sort by 'Bank_ID'
+    results_df = pd.DataFrame(results).sort_values(by=['bank_ID'])
+    
+    return results_df
+
+
+
+def final_statistic_table(bank_losses_assets, uninsured_deposit_mm_asset, insured_deposit_coverage=None, index_name = 'All Banks'):
+    # Merge the DataFrames on bank_name and Bank_ID to include uninsured deposit/MM Asset ratios and insured deposit coverage ratios
+    
+    
+    bank_count = len(bank_losses_assets.index)
+
+    final_stats = pd.DataFrame({
+        'Aggregate Loss': [f"{-round(bank_losses_assets['total_loss'].sum() / 1e9, 1)}T"],  # Convert to trillions
+        'Bank Level Loss': [f"{-round(bank_losses_assets['total_loss'].median() / 1e3, 1)}M"],  # Convert to millions
+        'Bank Level Loss Std': [f"{round(bank_losses_assets['total_loss'].std() / 1e6, 2)}B"],  # Std deviation for Bank Level Loss
+        'Share RMBS': [round(bank_losses_assets['Share RMBs'].median() * 100, 1)],  # Median percentage
+        'Share RMBS Std': [round(bank_losses_assets['Share RMBs'].std() * 100, 1)],  # Std deviation for Share RMBS
+        'Share Treasury and Other': [round(bank_losses_assets['Share Treasury and Other'].median() * 100, 1)],  # Median percentage
+        'Share Treasury and Other Std': [round(bank_losses_assets['Share Treasury and Other'].std() * 100, 1)],  # Std deviation
+        'Share Residential Mortgage': [round(bank_losses_assets['Share Residential Mortgage'].median() * 100, 1)],  # Median percentage
+        'Share Residential Mortgage Std': [round(bank_losses_assets['Share Residential Mortgage'].std() * 100, 1)],  # Std deviation
+        'Share Other Loan': [round(bank_losses_assets['Share Other Loan'].median() * 100, 1)],  # Median percentage
+        'Share Other Loan Std': [round(bank_losses_assets['Share Other Loan'].std() * 100, 1)],  # Std deviation
+        'Loss/Asset': [round(bank_losses_assets['loss/gross_asset'].median() * 100, 1)],  # Median percentage
+        'Loss/Asset Std': [round(bank_losses_assets['loss/gross_asset'].std() * 100, 1)],  # Std deviation
+        'Uninsured Deposit/MM Asset': [round(uninsured_deposit_mm_asset['Uninsured_Deposit_MM_Asset'].median() * 100, 1)],  # Median percentage
+        'Uninsured Deposit/MM Asset Std': [round(uninsured_deposit_mm_asset['Uninsured_Deposit_MM_Asset'].std() * 100, 1)],  # Std deviation
+        # 'Insured Deposit Coverage Ratio': [round(insured_deposit_coverage['insured_deposit_coverage_ratio'].median() * 100, 1)],  # Median percentage
+        # 'Insured Deposit Coverage Ratio Std': [round(insured_deposit_coverage['insured_deposit_coverage_ratio'].std() * 100, 1)],  # Std deviation
+        'Number of Banks': [len(bank_losses_assets.index.unique())]  # Count of unique banks
+    })
+
+    # Rename index to 'All Banks'
+    final_stats.index = [index_name]
+
+    final_stats = final_stats.T
+    
+    return final_stats
